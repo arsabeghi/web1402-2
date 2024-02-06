dc79e516124317fdb9d1331fe92f31ff3691a3ea
Alireza <alisabeghi1380@gmail.com>
2024-02-06 17:13:40 +0330

    fir add 2

---
+++ 01/Class.js
@@ -0,0 +1,16 @@
+console.log("hello");
+let a = 1;
+let b = 2;
+console.log("sum a and" , typeof X, "and value x" , X);
+X = "one";
+console.log("typeof x is" , typeof X, "and value x" , X);
+X = [1,2, "three"];
+console.log("typeof x is" , typeof X, "and value x" , X);
+console.log("member 0 of x is" , x[0]);
+X = {
+    mykey: 111,
+    myotherkey: "two"
+};
+console.log("typeof x is" , typeof X, "and value of x is" , X);
+console.log('x["mykey"] is' , x["mykey"]);
+console.log('x["mykey"] is' , x.mykey);
