d6501a3cd19e08a688128be9342a50987593f20e
Alireza Sabeghi <Alisabeghi1380@gmail.com>
2024-02-13 17:24:14 +0330

    Add files via upload
    02b

---
+++ 02b.js
@@ -0,0 +1,22 @@
+let inputs = [
+    {
+        name: nasser,
+        age: 18,
+
+    }
+    {
+        name: reza,
+        age: 18,
+
+    }
+    {
+        name: behrooz,
+        age: 18,
+
+    }
+
+]
+
+for(i = 0; i<inputs.length; i++){
+    console.log("property" , inputs[i] , "of object is" , obj[inputs[i]])
+}
